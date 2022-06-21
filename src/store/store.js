776968e4ec2086d88291c7cfdf48de302f540010
Enminsk <enminsk@yandex.ru>
2022-06-22 00:23:59 +0300

    refactor(category_page): refactor, not work server

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { reducer as categoriesReducer } from './sliceCategories'
+import { reducer as categoriesReducer } from './categoriesSlice';
 
 
 const rootReducer = combineReducers({
